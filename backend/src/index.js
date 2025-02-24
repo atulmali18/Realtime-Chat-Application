2654a51fd7c97cfe7e7d89288e22bd0627ed1f70
Atul Mali <atulmali102@gmail.com>
2025-02-24 13:32:59 +0530

    Project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser'; 
 import cors from 'cors';
 
+import path from "path";
 
 
 import authRoutes from './routes/auth.routes.js';
@@ -12,9 +13,10 @@ import { connectDB } from './lib/db.js';
 import { app, server } from './lib/socket.js';
 
 dotenv.config();
-app
+
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(cookieParser());
 app.use(express.json({limit: "10mb"}));
@@ -26,7 +28,15 @@ app.use(cors({
 
 
 app.use('/api/auth/', authRoutes);
-app.use('/api/messages', messageRoutes)
+app.use('/api/messages', messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 
 server.listen(PORT, () => {
