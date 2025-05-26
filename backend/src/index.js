f126fe5fd771813ceebd1bf38872c7b4c1d6b74e
Atul Mali <137202196+atulmali18@users.noreply.github.com>
2025-05-26 22:17:50 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,9 +22,9 @@ app.use(cookieParser());
 app.use(express.json({limit: "10mb"}));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cors({
-  origin: process.env.CLIENT_URL,
-  credentials: true
-}));
+    origin: "http://localhost:5173",
+    credentials: true,
+  }));
 
 
 app.use('/api/auth/', authRoutes);
