866438576e38a5bd85c91f4b7992d1d6c1a35a91
Atul Mali <137202196+atulmali18@users.noreply.github.com>
2025-05-27 09:43:10 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://chatify-gj1p.onrender.com/",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://chatify-gj1p.onrender.com/api",
   withCredentials: true,
 });
