3bf151690146805e456430168a7a7588f418ada4
Atul Mali <137202196+atulmali18@users.noreply.github.com>
2025-05-27 11:05:24 +0530

    Update socket.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -5,10 +5,12 @@ import express from "express";
 const app = express();
 const server = http.createServer(app);
 
-const io = new Server(server, {
+const io = socketIO(server, {
   cors: {
-    origin: ["http://localhost:5173"],
-  },
+    origin: "https://realtime-chat-application-oth1rn8ek-atulmali18s-projects.vercel.app",
+    methods: ["GET", "POST"],
+    credentials: true
+  }
 });
 
 export function getReceiverSocketId(userId) {
